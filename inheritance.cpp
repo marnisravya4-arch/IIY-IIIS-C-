0596e90de64fc4cf3b690ba710ddca8b90abaaac
marnisravya4-arch <marnisravya4@gmail.com>
2025-10-20 21:15:28 +0530

    Add files via upload

---
+++ inheritance.cpp
@@ -0,0 +1,19 @@
+#include<iostream> 
+using namespace std; 
+class vehicle{ 
+ public: 
+  vehicle(){ 
+  cout<<"This is a vehicle"<<endl;}
+  }; 
+class car:public vehicle 
+{ 
+ public:
+        car(){ 
+   cout<<"This vehicle is a car"<<endl;
+   }
+   }; 
+int main() 
+{ 
+ car obj; 
+ return 0; 
+} 
